066ecaf66811a77fe2dc49bee05d1f9982d568ab
Siddhartha Devkota <110275450+siddevkota@users.noreply.github.com>
2023-10-10 05:29:47 +0545

    Delete lib directory

--- lib/services/MAP/animation.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/services/MAP/distancecalc.dart';
-import 'package:latlong2/latlong.dart';
-
-class Animation {
-  MAPDISTANCE mapdistance = MAPDISTANCE();
-  
-}
